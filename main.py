7457db07b61daaafb13d65882d2c028f7300ea7b
Michael Huang <michaelhuang88@Outlook.com>
2018-08-01 22:19:55 -0400

    added slider for sim speed, fixed bugs

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from kivy.config import Config
 from kivy.graphics import *
 from kivy.uix.widget import Widget
 from kivy.clock import Clock
+from kivy.uix.slider import Slider
 from random import randint
 from random import shuffle
 from functools import partial
@@ -57,6 +58,7 @@ queue = []
 stack = [0 for x in range(width*height)]
 queueInd = [0,0]
 stackInd = [0]
+speed = [50]
 
 #change node colour
 def paint(x,y,self):
@@ -64,7 +66,7 @@ def paint(x,y,self):
     vert = grid[0][0].vert
     gridx = int(x//(hori+1))
     gridy = int(y//(vert+1))
-    if gridy>=(len(grid)) or gridx >= len(grid[0]) or gridy < 0 or gridx < 0 or grid[gridy][gridx].col==colour[0]:
+    if gridy>=(len(grid)) or gridx >= len(grid[0]) or gridy < 0 or gridx < 0:
         return
     with self.canvas:
         grid[gridy][gridx].col = colour[0]
@@ -75,7 +77,6 @@ def paint(x,y,self):
 
 def resetGrid(self):
     self.canvas.clear()
-    self.canvas.children = [widget.canvas for widget in self.children]
     with self.canvas:
         Color(.501,.501,.501,1)
         Rectangle(pos=(0,0),size=(900,540))
@@ -83,9 +84,17 @@ def resetGrid(self):
             for j in i:
                 Color(*colours[j.col])
                 Rectangle(pos=(j.x,j.y),size=(j.hori,j.vert))
+    self.clear_widgets()
+    self.add_widget(Touch())
+    self.add_widget(ToolBar())
+    select = speedSelect(value=speed[0])
+    select.update()
+    self.add_widget(select)
 
 
 def runPath(self,*largs):
+    if not running[0]:
+        stackInd[0] = -1
     if stackInd[0]<0:
         return
     y,x = stack[stackInd[0]]
@@ -94,7 +103,7 @@ def runPath(self,*largs):
         with self.canvas:
             Color(0,0,1,1)
             Rectangle(pos=(grid[y][x].x, grid[y][x].y), size=(grid[y][x].hori, grid[y][x].vert))
-    Clock.schedule_once(partial(runPath,self),1.0/60.0)
+    Clock.schedule_once(partial(runPath,self),(101-speed[0])/200)
 
 
 def bfsUpdate(self,*largs):
@@ -108,7 +117,7 @@ def bfsUpdate(self,*largs):
                 stackInd[0] += 1
                 stack[stackInd[0]] = backtrack[y][x]
                 y, x = backtrack[y][x]
-            Clock.schedule_once(partial(runPath, self), 1.0 / 60.0)
+            Clock.schedule_once(partial(runPath, self), (101-speed[0])/200)
             return
         if grid[y][x].col != 1:
             with self.canvas:
@@ -123,7 +132,7 @@ def bfsUpdate(self,*largs):
             checked[ny][nx] = 1
             backtrack[ny][nx] = [y,x]
             queue.append([ny,nx])
-    Clock.schedule_once(partial(bfsUpdate,self),1.0/60.0)
+    Clock.schedule_once(partial(bfsUpdate,self),(101-speed[0])/200)
 
 
 def bfs(self,*largs):
@@ -138,7 +147,7 @@ def bfs(self,*largs):
                 checked[i][j] = 1
             backtrack[i][j] = [-1,-1]
     if queue:
-        Clock.schedule_once(partial(bfsUpdate,self),1.0/60.0)
+        bfsUpdate(self)
 
 
 def dfsUpdate(self,*largs):
@@ -153,11 +162,12 @@ def dfsUpdate(self,*largs):
             stackInd[0] += 1
             stack[stackInd[0]] = backtrack[y][x]
             y,x = backtrack[y][x]
-        Clock.schedule_once(partial(runPath,self),1.0/60.0)
+        Clock.schedule_once(partial(runPath,self),(101-speed[0])/200)
         return
-    with self.canvas:
-        Color(0,1,1,1)
-        Rectangle(pos=(grid[y][x].x, grid[y][x].y), size=(grid[y][x].hori, grid[y][x].vert))
+    if grid[y][x].col != 1:
+        with self.canvas:
+            Color(0,1,1,1)
+            Rectangle(pos=(grid[y][x].x, grid[y][x].y), size=(grid[y][x].hori, grid[y][x].vert))
     shuffle(moves)
     for i in moves:
         ny,nx = y+i[0],x+i[1]
@@ -168,7 +178,7 @@ def dfsUpdate(self,*largs):
         queue.append([ny,nx])
         backtrack[ny][nx] = [y,x]
         checked[ny][nx] = 1
-    Clock.schedule_once(partial(dfsUpdate,self),1.0/60.0)
+    Clock.schedule_once(partial(dfsUpdate,self),(101-speed[0])/200)
 
 
 def dfs(self,*largs):
@@ -195,7 +205,7 @@ def dfs(self,*largs):
                     queue.append([ny, nx])
                     checked[ny][nx] = 1
     if queue:
-        Clock.schedule_once(partial(dfsUpdate,self),1.0/30.0)
+        dfsUpdate(self)
 
 
 class Touch(Widget):
@@ -209,6 +219,16 @@ class Touch(Widget):
         x = touch.x
         y = touch.y
         paint(x,y,self.parent)
+
+
+class speedSelect(Slider):
+
+    def changeSpeed(self,*args):
+        speed[0] = int(args[1])
+        self.lbl.text = str(int(args[1]))
+
+    def update(self):
+        self.lbl.text = str(speed[0])
 
 
 class ToolBar(BoxLayout):
@@ -232,50 +252,44 @@ class ToolBar(BoxLayout):
     def randomize(self):
         if running[0]:
             running[0] = 0
+            stackInd[0] = -1
             while queue:
                 queue.pop(-1)
-        resetGrid(self.parent)
         for i in grid:
             for j in i:
                 if randint(1,4)==1:
                     j.col = 0
                 else:
                     j.col = 3
-                with self.parent.canvas:
-                    Color(*colours[j.col])
-                    Rectangle(pos=(j.x, j.y), size=(j.hori, j.vert))
         y,x = randint(0,len(grid)-1),randint(0,len(grid[0])-1)
         a,b = randint(0,len(grid)-1),randint(0,len(grid[0])-1)
         while a==y and b==x:
             a, b = randint(0, len(grid) - 1), randint(0, len(grid[0]) - 1)
         grid[y][x].col = 1
         grid[a][b].col = 2
-        with self.parent.canvas:
-            Color(*colours[1])
-            Rectangle(pos=(grid[y][x].x, grid[y][x].y), size=(grid[y][x].hori, grid[y][x].vert))
-            Color(*colours[2])
-            Rectangle(pos=(grid[a][b].x, grid[a][b].y), size=(grid[a][b].hori, grid[a][b].vert))
+        resetGrid(self.parent)
 
     def clear(self):
         if running[0]:
             running[0] = 0
+            stackInd[0] = -1
             while queue:
                 queue.pop(-1)
+        for i in grid:
+            for j in i:
+                j.col = 3
         resetGrid(self.parent)
-        with self.parent.canvas:
-            Color(1,1,1,1)
-            for i in grid:
-                for j in i:
-                    j.col = 3
-                    Rectangle(pos=(j.x, j.y), size=(j.hori, j.vert))
 
     def simulate(self):
+        while queue:
+            queue.pop(-1)
+        stackInd[0] = -1
         running[0] = 1
-        resetGrid(self.parent)
         if algo[0]==0:
-            Clock.schedule_once(partial(bfs,self.parent),1.0/30.0)
+            bfs(self.parent)
         else:
-            Clock.schedule_once(partial(dfs,self.parent),1.0/30.0)
+            dfs(self.parent)
+        resetGrid(self.parent)
 
 
 class MainApp(App):
@@ -283,6 +297,7 @@ class MainApp(App):
         parent = Widget()
         layout = Touch()
         tools = ToolBar()
+        select = speedSelect(value=speed[0])
         with layout.canvas:
             Color(.501,.501,.501,1)
             Rectangle(pos=(0,0),size=(900,540))
@@ -293,6 +308,7 @@ class MainApp(App):
                     Rectangle(pos=(grid[i][j].x, grid[i][j].y), size=(grid[i][j].hori, grid[i][j].vert))
         parent.add_widget(layout)
         parent.add_widget(tools)
+        parent.add_widget(select)
         return parent
 
 
--- test3.py
+++ test3.py
@@ -7,6 +7,7 @@ from kivy.config import Config
 from kivy.graphics import *
 from kivy.uix.widget import Widget
 from kivy.clock import Clock
+from kivy.uix.slider import Slider
 from random import randint
 from random import shuffle
 from functools import partial
@@ -65,7 +66,7 @@ def paint(x,y,self):
     vert = grid[0][0].vert
     gridx = int(x//(hori+1))
     gridy = int(y//(vert+1))
-    if gridy>=(len(grid)) or gridx >= len(grid[0]) or gridy < 0 or gridx < 0 or grid[gridy][gridx].col==colour[0]:
+    if gridy>=(len(grid)) or gridx >= len(grid[0]) or gridy < 0 or gridx < 0:
         return
     with self.canvas:
         grid[gridy][gridx].col = colour[0]
@@ -86,6 +87,9 @@ def resetGrid(self):
     self.clear_widgets()
     self.add_widget(Touch())
     self.add_widget(ToolBar())
+    select = speedSelect(value=speed[0])
+    select.update()
+    self.add_widget(select)
 
 
 def runPath(self,*largs):
@@ -99,7 +103,7 @@ def runPath(self,*largs):
         with self.canvas:
             Color(0,0,1,1)
             Rectangle(pos=(grid[y][x].x, grid[y][x].y), size=(grid[y][x].hori, grid[y][x].vert))
-    Clock.schedule_once(partial(runPath,self),1.0/speed[0])
+    Clock.schedule_once(partial(runPath,self),(101-speed[0])/200)
 
 
 def bfsUpdate(self,*largs):
@@ -113,7 +117,7 @@ def bfsUpdate(self,*largs):
                 stackInd[0] += 1
                 stack[stackInd[0]] = backtrack[y][x]
                 y, x = backtrack[y][x]
-            Clock.schedule_once(partial(runPath, self), 1.0 / speed[0])
+            Clock.schedule_once(partial(runPath, self), (101-speed[0])/200)
             return
         if grid[y][x].col != 1:
             with self.canvas:
@@ -128,7 +132,7 @@ def bfsUpdate(self,*largs):
             checked[ny][nx] = 1
             backtrack[ny][nx] = [y,x]
             queue.append([ny,nx])
-    Clock.schedule_once(partial(bfsUpdate,self),1.0/speed[0])
+    Clock.schedule_once(partial(bfsUpdate,self),(101-speed[0])/200)
 
 
 def bfs(self,*largs):
@@ -158,11 +162,12 @@ def dfsUpdate(self,*largs):
             stackInd[0] += 1
             stack[stackInd[0]] = backtrack[y][x]
             y,x = backtrack[y][x]
-        Clock.schedule_once(partial(runPath,self),1.0/speed[0])
+        Clock.schedule_once(partial(runPath,self),(101-speed[0])/200)
         return
-    with self.canvas:
-        Color(0,1,1,1)
-        Rectangle(pos=(grid[y][x].x, grid[y][x].y), size=(grid[y][x].hori, grid[y][x].vert))
+    if grid[y][x].col != 1:
+        with self.canvas:
+            Color(0,1,1,1)
+            Rectangle(pos=(grid[y][x].x, grid[y][x].y), size=(grid[y][x].hori, grid[y][x].vert))
     shuffle(moves)
     for i in moves:
         ny,nx = y+i[0],x+i[1]
@@ -173,7 +178,7 @@ def dfsUpdate(self,*largs):
         queue.append([ny,nx])
         backtrack[ny][nx] = [y,x]
         checked[ny][nx] = 1
-    Clock.schedule_once(partial(dfsUpdate,self),1.0/speed[0])
+    Clock.schedule_once(partial(dfsUpdate,self),(101-speed[0])/200)
 
 
 def dfs(self,*largs):
@@ -214,6 +219,16 @@ class Touch(Widget):
         x = touch.x
         y = touch.y
         paint(x,y,self.parent)
+
+
+class speedSelect(Slider):
+
+    def changeSpeed(self,*args):
+        speed[0] = int(args[1])
+        self.lbl.text = str(int(args[1]))
+
+    def update(self):
+        self.lbl.text = str(speed[0])
 
 
 class ToolBar(BoxLayout):
@@ -276,15 +291,13 @@ class ToolBar(BoxLayout):
             dfs(self.parent)
         resetGrid(self.parent)
 
-    def changeSpeed(self,*args):
-        speed[0] = int(args[1])
 
-
-class testApp(App):
+class MainApp(App):
     def build(self):
         parent = Widget()
         layout = Touch()
         tools = ToolBar()
+        select = speedSelect(value=speed[0])
         with layout.canvas:
             Color(.501,.501,.501,1)
             Rectangle(pos=(0,0),size=(900,540))
@@ -295,10 +308,11 @@ class testApp(App):
                     Rectangle(pos=(grid[i][j].x, grid[i][j].y), size=(grid[i][j].hori, grid[i][j].vert))
         parent.add_widget(layout)
         parent.add_widget(tools)
+        parent.add_widget(select)
         return parent
 
 
-gridsim = testApp()
+gridsim = MainApp()
 
 if __name__=='__main__':
     gridsim.run()
